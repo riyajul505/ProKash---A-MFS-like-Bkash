e97724e92622099a0cb20aa38f3008e47e20cc79
riyajul505 <rlriyajul@gmail.com>
2024-08-24 02:07:44 +0600

    login page designed

--- Client/src/Pages/Login/Login.jsx
+++ Client/src/Pages/Login/Login.jsx
@@ -1,81 +1,117 @@
 import {
-    Card,
-    Input,
-    Button,
-    CardBody,
-    CardHeader,
-    Typography,
-  } from "@material-tailwind/react";
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Input,
+  Typography,
+} from "@material-tailwind/react";
+import { useForm } from "react-hook-form";
+import { TbBrandProducthunt } from "react-icons/tb";
+
 const Login = () => {
-    return (
-        <div>
-             <Card
-      shadow={false}
-      className="md:px-24 md:py-14 py-8 border border-gray-300"
-    >
-      <CardHeader shadow={false} floated={false} className="text-center">
-        <Typography
-          variant="h1"
-          color="blue-gray"
-          className="mb-4 !text-3xl lg:text-4xl"
-        >
-          Web3 Login Simplified
-        </Typography>
-        <Typography className="!text-gray-600 text-[18px] font-normal md:max-w-sm">
-          Enjoy quick and secure access to your accounts on various Web3
-          platforms.
-        </Typography>
-      </CardHeader>
-      <CardBody>
-        <form
-          action="#"
-          className="flex flex-col gap-4 md:mt-12"
-        >
-          <div>
-            <label htmlFor="email">
-              <Typography
-                variant="small"
-                color="blue-gray"
-                className="block font-medium mb-2"
-              >
-                Your Email
-              </Typography>
-            </label>
-            <Input
-              id="email"
-              color="gray"
-              size="lg"
-              type="email"
-              name="email"
-              placeholder="name@mail.com"
-              className="!w-full placeholder:!opacity-100 focus:!border-t-primary !border-t-blue-gray-200"
-              labelProps={{
-                className: "hidden",
-              }}
-            />
-          </div>
-          <Button size="lg" color="gray" fullWidth>
-            continue
-          </Button>
-          
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <div className="flex justify-center bg-white h-[100vh]">
+      <Card
+        shadow={false}
+        className="md:px-24 md:py-14 py-8 border border-gray-300 lg:w-1/2 items-center justify-center"
+      >
+        <CardHeader shadow={false} floated={false} className="text-center">
           <Typography
-            variant="small"
-            className="text-center mx-auto max-w-[19rem] !font-medium !text-gray-600"
+            variant="h1"
+            color="blue-gray"
+            className="mb-4 !text-3xl lg:text-4xl"
           >
-            Upon signing in, you consent to abide by our{" "}
-            <a href="#" className="text-gray-900">
-              Terms of Service
-            </a>{" "}
-            &{" "}
-            <a href="#" className="text-gray-900">
-              Privacy Policy.
-            </a>
+            <span className="flex flex-row justify-center items-center">
+              <TbBrandProducthunt className="text-4xl text-green-400" />
+              rokash
+            </span>{" "}
+            Login
           </Typography>
-        </form>
-      </CardBody>
-    </Card>
-        </div>
-    );
+          <Typography className="!text-gray-600 text-[18px] font-normal md:max-w-sm">
+            Enjoy quick and secure access to your accounts on various Web3
+            platforms.
+          </Typography>
+        </CardHeader>
+        <CardBody>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-4 md:mt-12"
+          >
+            <div>
+              <label htmlFor="email">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="block font-medium mb-2"
+                >
+                  Number
+                </Typography>
+              </label>
+              <input
+                id="number"
+                color="gray"
+                {...register("number", { required: true })}
+                max={11}
+                min={11}
+                type="number"
+                placeholder="01871111324"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </div>
+            <div>
+              <label htmlFor="email">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="block font-medium mb-2"
+                >
+                  Pin
+                </Typography>
+              </label>
+              <Input
+                id="email"
+                color="gray"
+                size="lg"
+                type="number"
+                max={4}
+                min={4}
+                {...register("pin", { required: true })}
+                inputMode="numeric"
+                name="pin"
+                placeholder="1234"
+                className="!w-full placeholder:!opacity-100 focus:!border-t-primary !border-t-blue-gray-200 "
+                labelProps={{
+                  className: "hidden",
+                }}
+              />
+            </div>
+            <Button size="lg" color="gray" fullWidth type="submit">
+              Continue
+            </Button>
+
+            <Typography
+              variant="small"
+              className="text-center mx-auto max-w-[19rem] !font-medium !text-gray-600"
+            >
+              Upon signing in, you consent to abide by our{" "}
+              <a href="#" className="text-gray-900">
+                Terms of Service
+              </a>{" "}
+              &{" "}
+              <a href="#" className="text-gray-900">
+                Privacy Policy.
+              </a>
+            </Typography>
+          </form>
+        </CardBody>
+      </Card>
+    </div>
+  );
 };
 
 export default Login;
