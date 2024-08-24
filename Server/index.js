2c0aa7cdd99abbf0bdeba5d6f057904c86ebb6e8
riyajul505 <rlriyajul@gmail.com>
2024-08-24 17:21:12 +0600

    registration api building

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,11 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const bcrypt = require('bcrypt');
+const saltRounds = 11;
+const salt = bcrypt.genSaltSync(saltRounds);
 require("dotenv").config();
 
 const app = express();
@@ -12,6 +16,48 @@ const port = process.env.port || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+// mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ixzkh9v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // collection names
+    const usersCollection = client.db("Prokash").collection("Users-Collection");
+    // registration
+    app.post("/registration", async (req, res) => {
+      const userInfo = req.body;
+      const {pin} = userInfo;
+      const exist = await usersCollection.findOne({mobile_number: userInfo.mobile_number});
+      console.log(exist);
+      // if(exist){
+      //   res.send({message: 'User already exist'})
+      // }
+      // else {
+      //   const hashPin = bcrypt.hashSync(pin, salt);
+      //   userInfo.pin = hashPin;
+      //   const result = await usersCollection.insertOne(userInfo);
+      //   res.send(result);
+      // }
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Server running successfully");
